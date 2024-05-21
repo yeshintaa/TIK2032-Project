27e7256bab5f5825014aa4c97113af61a3e66fdc
yeshintaa <152085399+yeshintaa@users.noreply.github.com>
2024-05-21 20:14:26 +0800

    Update blog.php

--- blog.php
+++ blog.php
@@ -6,47 +6,49 @@ $result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <link rel="stylesheet" href="index.css">
-</head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blogs</title>
+  <link rel="stylesheet" href="style.css">
+  </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li class="link"><a href="index.php">Home</a></li>
-                <li class="link"><a href="gallery.php">Gallery</a></li>
-                <li class="link"><a href="blog.php">Blog</a></li>
-                <li class="link"><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
 
-    <main class="blog">
-        <div class="containerblog">
+<header>
+  <h1>Blogs</h1>
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="gallery.php">Gallery</a></li>
+      <li><a href="blog.php">Blog</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+
+<main class="blog">
+    <div class="container section">
+        <div class="section-cards">
             <?php
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
                     ?>
-                    <div class="cardblog">
-                        <div class="contentblog">
-                            <h3><?php echo $row["title"]; ?></h3>
-                            <p><?php echo $row["content"]; ?></p>
-                        </div>
+                    <div class="section-card">
+                        <span><?php echo substr($row["title"], 0, 1); ?></span>
+                        <h2><?php echo $row["title"]; ?></h2>
+                        <p><?php echo $row["content"]; ?></p>
                     </div>
                     <?php
                 }
             } else {
-                echo "Tidak ada artikel yang ditemukan.";
+                echo "<p>Tidak ada artikel yang ditemukan.</p>";
             }
             $conn->close();
             ?>
         </div>
-    </main>
+    </div>
+</main>
 
-    <?php include 'includes/footer.php'; ?>
 </body>
 </html>
