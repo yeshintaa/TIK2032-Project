00d9769911986eeaf5c01cfc8eca27bac6e1f3f0
yeshintaa <152085399+yeshintaa@users.noreply.github.com>
2024-05-21 20:26:35 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Ganti dengan username MySQL Anda
-$password = ""; // Ganti dengan password MySQL Anda
-$dbname = "pemrodb"; // Ganti dengan nama database Anda
+$username = "root"; 
+$password = ""; 
+$dbname = "pemrodb"; 
 
-// Buat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Periksa koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
